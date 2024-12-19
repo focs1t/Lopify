d37838a0bd390937df7b7bc6e79f5159f0928fba
focsit <f0csit@mail.ru>
2024-12-19 03:24:57 +0300

    adding user profile func

---
+++ mobile/app/src/main/java/ru/focsit/mobile/fragments/user/UserProfileCommentsAdapter.kt
@@ -0,0 +1,33 @@
+package ru.focsit.mobile.fragments.user
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.focsit.mobile.R
+import ru.focsit.mobile.data.CommentDto
+
+class UserProfileCommentsAdapter(private val comments: List<CommentDto>) :
+    RecyclerView.Adapter<UserProfileCommentsAdapter.CommentViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user_comment, parent, false)
+        return CommentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        val comment = comments[position]
+        holder.bind(comment)
+    }
+
+    override fun getItemCount(): Int = comments.size
+
+    inner class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val commentText: TextView = itemView.findViewById(R.id.commentText)
+
+        fun bind(comment: CommentDto) {
+            commentText.text = comment.content
+        }
+    }
+}
